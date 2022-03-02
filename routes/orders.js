e666997ae5a954fc5185fe33805be3cc9ef4c6e1
Maaz Hussain <maazhussain097@gmail.com>
2022-03-02 18:03:11 +0500

    order email sent

--- routes/orders.js
+++ routes/orders.js
@@ -1,4 +1,5 @@
 const { Orders } = require("../models/orders");
+const { Project } = require("../models/project");
 const express = require("express");
 const router = express.Router();
 const moment = require("moment");
@@ -6,6 +7,21 @@ const auth = require("../middleware/auth");
 const keys = require("../config/dev");
 const nodemailer = require("nodemailer");
 const config = require("config");
+const fs = require("fs");
+const { createCanvas, loadImage } = require("canvas");
+var handlebars = require("handlebars");
+
+var readHTMLFile = function (path, callback) {
+  fs.readFile(path, { encoding: "utf-8" }, function (err, html) {
+    if (err) {
+      callback(err);
+      throw err;
+    } else {
+      callback(null, html);
+    }
+  });
+};
+
 //Add new Order
 router.post("/", auth, async (req, res) => {
   try {
@@ -21,28 +37,6 @@ router.post("/", auth, async (req, res) => {
     });
     const newSavedOrder = await order.save();
     res.send(newSavedOrder);
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: keys.email,
-        pass: keys.password,
-      },
-    });
-
-    const mailOptions = {
-      from: keys.email,
-      to: `${req.user.email}`,
-      subject: "Tappr PTY LTD - Order Successfull",
-      html: "Your Order Successfull With Receipt No " + ` ${newSavedOrder.receiptNo} `,
-    };
-
-    transporter.sendMail(mailOptions, (err, response) => {
-      if (err) {
-        res.status(400).send({ message: "Server error" });
-      } else {
-        res.status(200).send({ message: newSavedOrder });
-      }
-    });
   } catch (err) {
     res.status(409).send({ message: err.message });
   }
@@ -126,6 +120,88 @@ router.get("/resendEmail/:orderId", auth, async (req, res) => {
       });
     } else {
       res.status(400).send({ message: "No Order Present" });
+    }
+  } catch (err) {
+    res.status(409).send({ message: err.message });
+  }
+});
+
+//Send Email of Project Details
+router.post("/sendProjectDetail/:orderId", auth, async (req, res) => {
+  const orderId = req.params.orderId;
+  const body = req.body;
+  try {
+    if (!body.to) return res.status(400).send({ message: "to is not Present in data" });
+    const order = await Orders.findById(orderId);
+    if (order) {
+      const projectId = order.projectId;
+      const project = await Project.findById(projectId);
+
+      if (project) {
+        // get the base64 string
+        var base64String = project.image.replace(/^data:image\/[a-z]+;base64,/, "");
+        const fileName = `${Date.now()}-tappr-projectFile.pdf`;
+        const path = `files/${fileName}`;
+
+        await loadImage(Buffer.from(base64String, "base64")).then(async (img) => {
+          const canvas = createCanvas(img.width, img.height, "pdf");
+          const ctx = canvas.getContext("2d");
+
+          ctx.drawImage(img, 0, 0, img.width, img.height);
+          const base64image = canvas.toBuffer();
+          // write the file to a pdf file
+          fs.writeFile(path, base64image, function (err) {
+            console.log("File created");
+          });
+        });
+        console.log("hello2");
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: keys.email,
+            pass: keys.password,
+          },
+        });
+        readHTMLFile("receipt.html", function (err, html) {
+          var template = handlebars.compile(html);
+          var replacements = {
+            username: "Tappr",
+            receiptNo: order.receiptNo,
+            createdDate: moment(Date.now()).format("MMMM D, YYYY"),
+            time: moment(Date.now()).format("hh:mm"),
+
+            item: fileName,
+            amount: `$${order.amount}`,
+            total: `$${order.amount}`,
+          };
+          var htmlToSend = template(replacements);
+          const mailOptions = {
+            from: keys.email,
+            to: `${req.body.to}`,
+            cc: req.body.cc,
+            subject: `Tappr PTY LTD - Project File Receipt No ${order.receiptNo}`,
+            html: htmlToSend,
+            attachments: [
+              {
+                path: path,
+              },
+            ],
+          };
+          transporter.sendMail(mailOptions, (err, response) => {
+            if (err) {
+              fs.unlinkSync(`./${path}`);
+              return res.status(400).send({ message: "Server error" });
+            } else {
+              fs.unlinkSync(`./${path}`);
+              return res.status(200).json({ message: "Email Sent" });
+            }
+          });
+        });
+      } else {
+        return res.status(400).send({ message: "Project Is Not Present" });
+      }
+    } else {
+      return res.status(400).send({ message: "Invalid Order Id" });
     }
   } catch (err) {
     res.status(409).send({ message: err.message });
--- routes/project.js
+++ routes/project.js
@@ -3,11 +3,6 @@ const express = require("express");
 const moment = require("moment");
 const router = express.Router();
 const { Project, validateProject } = require("../models/project");
-const keys = require("../config/dev");
-const nodemailer = require("nodemailer");
-const config = require("config");
-const fs = require("fs");
-const { createCanvas, loadImage } = require("canvas");
 
 // Get All Projects
 router.get("/", auth, async (req, res) => {
@@ -25,7 +20,6 @@ router.get("/", auth, async (req, res) => {
 router.get("/:id", auth, async (req, res) => {
   try {
     let project = await Project.findOne({ _id: req.params.id });
-
     if (project.userId === req.user._id) {
       res.status(200).send(project);
     } else {
@@ -119,67 +113,6 @@ router.get("/search/:filterWord", auth, async (req, res) => {
         const project = await Project.find({ planNo: { $regex: filter, $options: "i" }, userId: req.user._id });
         res.status(200).send(project);
       }
-    }
-  } catch (err) {
-    res.status(409).send({ message: err.message });
-  }
-});
-
-//Send Email of Project Details
-router.post("/sendProjectDetail/:projectId", auth, async (req, res) => {
-  const projectId = req.params.projectId;
-  try {
-    if (!projectId) res.status(400).send({ message: "Project Id is not present" });
-
-    const project = await Project.findById(projectId);
-
-    // get the base64 string
-    var base64String = project.image.replace(/^data:image\/[a-z]+;base64,/, "");
-    const fileName = `${Date.now()}-tappr-projectFile.pdf`;
-    const path = `files/${fileName}`;
-
-    await loadImage(Buffer.from(base64String, "base64")).then(async (img) => {
-      const canvas = createCanvas(img.width, img.height, "pdf");
-      const ctx = canvas.getContext("2d");
-
-      ctx.drawImage(img, 0, 0, img.width, img.height);
-      const base64image = canvas.toBuffer();
-      // write the file to a pdf file
-      await fs.writeFile(path, base64image, function (err) {
-        console.log("File created");
-      });
-    });
-    if (project) {
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: keys.email,
-          pass: keys.password,
-        },
-      });
-
-      const mailOptions = {
-        from: keys.email,
-        to: `${req.body.to}`,
-        cc: req.body.cc,
-        subject: "Tappr PTY LTD - Project Created",
-        html: "Your Project is Created With\n\n" + `Id ${project._id} `,
-        attachments: [
-          {
-            // utf-8 string as an attachment
-            path: path,
-          },
-        ],
-      };
-      transporter.sendMail(mailOptions, (err, response) => {
-        if (err) {
-          fs.unlinkSync(`./${path}`);
-          return res.status(400).send({ message: "Server error" });
-        } else {
-          fs.unlinkSync(`./${path}`);
-          return res.status(200).json({ message: "Email Sent" });
-        }
-      });
     }
   } catch (err) {
     res.status(409).send({ message: err.message });
