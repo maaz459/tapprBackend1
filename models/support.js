aa9a286791133023d77ab6488f46cb0cdaff4958
Maaz Hussain <maazhussain097@gmail.com>
2022-03-03 15:25:49 +0500

    enum added in support

--- models/support.js
+++ models/support.js
@@ -15,14 +15,13 @@ const FAQ = mongoose.model("FAQ", faqSchema);
 
 const supportSchema = new mongoose.Schema({
   issue: { type: String },
-  connectType: { type: String, enum: ["EMAIL", "PHONE"] },
+  connectType: { type: String, enum: ["EMAIL", "PHONE", "MESSAGE"] },
   description: { type: String },
   screens: { type: [String], value: [String] },
-  email: { type: String},
+  email: { type: String },
   userId: { type: String, required: true },
   publishDate: { type: String },
   status: { type: String },
-
 });
 const supportTicket = mongoose.model("supportTicket", supportSchema);
 
